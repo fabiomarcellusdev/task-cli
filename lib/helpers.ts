b65c84c922f2db46cc696918c81ead24e05231fa
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-09 09:16:55 -0500

    Document helper functions + add types where needed

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -17,13 +17,20 @@ export const isHelpCommandType = (value: any): value is HelpCommandType => {
     return ["add", "update", "delete", "mark-in-progress", "mark-done", "list"].includes(value);
 };
 
-export function ensureTasksFileExists() {
+/**
+ * Ensures that the tasks file exists. If it doesn't, it's creates and contains an empty array.
+ */
+export function ensureTasksFileExists(): void {
     if(!fs.existsSync(TASKS_FILE)) {
-        fs.writeFileSync(TASKS_FILE, JSON.stringify([]), "utf8"); //  utf8 is a widely used encoding that can represent every character in the Unicode character set. 
+        fs.writeFileSync(TASKS_FILE, JSON.stringify([]), "utf8"); 
     }
 }
 
-export const showHelp = (cmd?: HelpCommandType) => {
+/**
+ * Displays help information for all or specified command.
+ * @param cmd Help {cmd}
+ */
+export const showHelp = (cmd?: HelpCommandType): void => {
     const helpMessages = {
         add: "add <task description> - Adds a new task with the given description.",
         update: "update <task ID> <status> - Updates the status of the task with the given ID.",
@@ -59,18 +66,27 @@ export const showHelp = (cmd?: HelpCommandType) => {
     }
 };
 
-// Load tasks from file
-const loadTasks = () => {
+/**
+ * Load tasks data from TASKS_FILE file
+ * @returns Array of task objects
+ */
+const loadTasks = (): Task[] => {
     const taskData = fs.readFileSync(TASKS_FILE, "utf8");
     return JSON.parse(taskData);
 }
 
-// Save tasks to file
+/**
+ * Saves tasks data to TASKS_FILE file
+ * @param tasks Array of task objects
+ */
 const saveTasks = (tasks: Task[]) => {
     fs.writeFileSync(TASKS_FILE, JSON.stringify(tasks), "utf8");
 };
 
-// Generate a unique task ID
+/**
+ * 
+ * @returns Unique task ID based on current Date
+ */
 const generateTaskId = () => {
     return Date.now().toString();
 }
