f6c69abdccb88aa8504f925fe152330c7f1a5ec1
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-11 17:28:29 -0500

    Refactor showHelp.ts

--- lib/showHelp.ts
+++ lib/showHelp.ts
@@ -1,30 +1,22 @@
 import { HelpCommandType } from "./helpers";
 
-/**
- * Displays help information for all or specified command.
- * @param cmd Help {cmd}
- */
-export const showHelp = (cmd?: HelpCommandType): void => {
-    const helpMessages = {
-        add: "add <task description> - Adds a new task with the given description.",
-        update: "update <task ID> <description> - Updates the description of the task with the given ID.",
-        delete: "delete <task ID> - Deletes the task with the given ID.",
-        mark: "mark <task ID> <status> - Updates task to have new status.",
-        list: "list <status or all> - Lists all tasks, optionally filtered by status.",
-    };
+export const HELP_MESSAGES_PER_COMMAND = {
+    add: "add <task description> - Adds a new task with the given description.",
+    update: "update <task ID> <description> - Updates the description of the task with the given ID.",
+    delete: "delete <task ID> - Deletes the task with the given ID.",
+    mark: "mark <task ID> <status> - Updates task to have new status.",
+    list: "list <status or all> - Lists all tasks, optionally filtered by status.",
+};
 
-    if (cmd && helpMessages[cmd]) {
-        console.log(helpMessages[cmd]);
-    } else {
-        console.log(`
+export const HELP_ALL_COMMANDS = `
             Usage: task-cli <command> [options]
             
             Commands:
-              ${helpMessages.add}
-              ${helpMessages.update}
-              ${helpMessages.delete}
-              ${helpMessages.mark}
-              ${helpMessages.list}
+              ${HELP_MESSAGES_PER_COMMAND.add}
+              ${HELP_MESSAGES_PER_COMMAND.update}
+              ${HELP_MESSAGES_PER_COMMAND.delete}
+              ${HELP_MESSAGES_PER_COMMAND.mark}
+              ${HELP_MESSAGES_PER_COMMAND.list}
             
             Examples:
               task-cli add "Buy groceries"
@@ -32,8 +24,19 @@ export const showHelp = (cmd?: HelpCommandType): void => {
               task-cli delete 12345
               task-cli mark 12345 in-progress
               task-cli list in-progress
-              task-cli help add
-                    `);
+              task-cli help add`;
+
+/**
+ * Displays help information for all or specified command.
+ * @param cmd Help {cmd}
+ */
+export const showHelp = (cmd?: HelpCommandType): void => {
+    
+
+    if (cmd && HELP_MESSAGES_PER_COMMAND[cmd]) {
+        console.log(HELP_MESSAGES_PER_COMMAND[cmd]);
+    } else {
+        console.log(HELP_ALL_COMMANDS);
     }
 };
 
